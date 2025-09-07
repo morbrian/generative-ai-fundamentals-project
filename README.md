dcd112bf3a399e9808883a95d25d88518239e0c2
Brian Moriarty <morbrian@me.com>
2025-09-07 11:38:26 -0700

    python environment setup

--- README.md
+++ README.md
@@ -7,6 +7,15 @@
 Using `pyenv` is recommended for managing multiple verions of python on same system.
 
 ```
+# get prereq packages
+sudo apt update
+sudo apt install -y \
+  make build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+  libffi-dev liblzma-dev
+
+# install pyenv
 curl https://pyenv.run | bash
 ```
 
--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-export PY_VERSION=3.10.11
+export PYTHON_VERSION=3.10.11
 
 # Set up Python version (if using pyenv)
-pyenv versions | grep -q "$PY_VERSION" || pyenv install $PY_VERSION
-pyenv local $PY_VERSION
+if pyenv versions --bare | grep -qx "$PYTHON_VERSION"; then
+  echo "Python $PYTHON_VERSION is already installed."
+else
+  echo "Installing Python $PYTHON_VERSION..."
+  pyenv install "$PYTHON_VERSION"
+fi
+
+pyenv local $PYTHON_VERSION
 
 # Create and activate virtual environment
 python -m venv venv
